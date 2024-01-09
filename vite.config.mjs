3098bf16d44d62bad715c3f62008b6f34c8bc0d4
Bryan Buchs <bryan@kapowinc.com>
2024-01-09 14:34:26 -0600

    Use glob.sync for LESS vars, test `vite build`

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,17 +2,19 @@ import { defineConfig } from 'vite'
 import { join } from 'node:path'
 import lessPluginGlob from 'less-plugin-glob'
 import twig from 'vite-plugin-twig-drupal'
-
 import { glob } from 'glob'
 
-const variables = await glob('./less/**/*.less')
+// const variables = await glob('less/**/*.less')
 
 export default defineConfig({
   // build: {
   //   rollupOptions: {
-  //     // overwrite default .html entry
-  //     input: './components/**/*.library.js',
-  //   },
+  //     input: glob.sync('components/**/*.library.js'),
+  //     output: {
+  //       dir: 'output',
+  //       entryFileNames: '[name]/[name].js'
+  //     }
+  //   }
   // },
   plugins: [
     twig({
@@ -27,7 +29,7 @@ export default defineConfig({
         lessOptions: {
           plugins: [lessPluginGlob]
         },
-        additionalData: variables.map(file => `@import '${file}';`).join(' ')
+        additionalData: glob.sync('less/**/*.less').map(file => `@import '${file}';`).join(' ')
       }
     }
   }
