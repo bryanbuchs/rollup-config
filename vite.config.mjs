1b0634da4b7739ccc9564391e1abdf7d349f3aa6
Bryan Buchs <bryan@kapowinc.com>
2024-01-15 13:15:16 -0600

    wip

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,15 +7,36 @@ import { glob } from 'glob'
 // const variables = await glob('less/**/*.less')
 
 export default defineConfig({
-  // build: {
-  //   rollupOptions: {
-  //     input: glob.sync('components/**/*.library.js'),
-  //     output: {
-  //       dir: 'output',
-  //       entryFileNames: '[name]/[name].js'
-  //     }
-  //   }
-  // },
+  build: {
+    rollupOptions: {
+      input: glob.sync('components/**/*.library.js'),
+      // output: {
+      //   dir: 'output',
+      //   entryFileNames: '[name]/[name].js'
+      // }
+      // input: './src/media-video.library.js',
+      output: {
+        dir: 'output',
+        format: 'iife',
+        name: 'build',
+        assetFileNames: '[name]/[name][extname]'
+      },
+      plugins: [
+        styles({
+          mode: ['extract'],
+          less: {
+            plugins: [lessPluginGlob]
+          }
+        }),
+        nodeResolve(),
+        commonjs(),
+        nodePolyfills(),
+        clear({
+          targets: ['build']
+        })
+      ]
+    }
+  },
   plugins: [
     twig({
       namespaces: {
@@ -29,7 +50,10 @@ export default defineConfig({
         lessOptions: {
           plugins: [lessPluginGlob]
         },
-        additionalData: glob.sync('less/**/*.less').map(file => `@import '${file}';`).join(' ')
+        additionalData: glob
+          .sync('less/**/*.less')
+          .map(file => `@import '${file}';`)
+          .join(' ')
       }
     }
   }
